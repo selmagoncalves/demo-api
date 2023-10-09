ed68d37c19eeb2614b7479a51e3d1e343e422fbc
selmagoncalves <selma.goncalves.stz@gmail.com>
2023-10-09 10:52:25 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/selma/demo/domain/Livro.java
@@ -0,0 +1,13 @@
+package com.selma.demo.domain;
+
+class Livro {
+	
+	private Integer id;
+	private String titulo;
+	private String nome_autor;
+	private String texto;
+	
+	private Categoria categoria;
+	
+	
+}
